d96a0d4aa2b13bdfd092f8dc10d02094d58a404a
Bertyn Boulikou <bboulikou@gmail.com>
2022-03-21 15:06:06 +0100

    mj name fichier user

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const user = require("./controller/user");
+const user = require("./controller/userController");
 const verifyToken = require("./services/verifyToken");
 
 exports.router = (function () {
