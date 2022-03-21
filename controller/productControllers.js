84148319462e665362ac27d6713f1dee4fa92ad1
Bertyn Boulikou <bboulikou@gmail.com>
2022-03-21 14:16:32 +0100

    structure controller

---
+++ controller/productControllers.js
@@ -0,0 +1,10 @@
+// product Controller
+
+const product = require("../db/schema/product");
+const db = require("../db/connexion");
+
+async function index(req, res) {}
+async function getByProductId(req, res) {}
+async function updateProduct(req, res) {}
+async function deleteProduct(req, res) {}
+module.exports = {};
