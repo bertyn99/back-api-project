79ec82cbd9e84ed6eea845d24242981428746d5f
Bertyn Boulikou <bboulikou@gmail.com>
2022-03-21 12:51:34 +0100

    ajout helmet

--- index.js
+++ index.js
@@ -3,12 +3,16 @@
 const express = require("express");
 const app = express();
 const compression = require("compression");
+const helmet = require("helmet");
 const config = require("./config");
+
 const apiRouter = require("./apiRoutes").router;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(helmet());
 app.use(compression());
+app.disable("x-powered-by");
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
