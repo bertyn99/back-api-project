a454c46fff9364bc711f644a39a52fb47ac346ee
VicAffile <victoraffile@gmail.com>
2022-03-29 10:23:55 +0200

    Add schema facture

---
+++ db/schema/bill.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const Schema = mongoose.Schema;
+const validator = require('validator')
+
+let billSchema = new Schema({
+    idUser: {
+        type: mongoose.ObjectId,
+        required: true,
+        ref: 'user'
+    },
+    emissionDate: {
+        type: Date,
+        required: true,
+    },
+    isPaid: {
+        type: bool,
+        required: true,
+    },
+    paidDate: {
+        type: Date,
+        required: true,
+    },
+    price: {
+        type: decimal,
+        required: true,
+    },
+    products: [{ type: mongoose.ObjectId }]
+}, {
+    timestamps: true
+});
+
+const Bill = mongoose.model('Bill', billSchema)
+
+module.exports = Bill;
