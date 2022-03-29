4df293547fa32aeb8817a878844631dbc2e06445
Bertyn Boulikou <bboulikou@gmail.com>
2022-03-29 14:33:02 +0200

    fix jwt token

--- controller/userController.js
+++ controller/userController.js
@@ -7,88 +7,87 @@ const database = require("../db/connexion");
 const bcrypt = require("bcrypt");
 
 async function register(req, res) {
+  const user = new User(req.body);
 
-    const user = new User(req.body);
-
-    try {
-        await user.save();
-        //envoyer l'email de confirmation de création de compte
-        const token = await user.generateAuthToken();
-        res.status(201).send({ user, token });
-    } catch (e) {
-        res.status(400).send(e);
-    }
+  try {
+    await user.save();
+    //envoyer l'email de confirmation de création de compte
+    const token = await user.generateAuthToken();
+    console.log({ user, token });
+    res.status(200).send({ user, token });
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
 }
 
 async function logIn(req, res) {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password);
-        const token = await user.generateAuthToken();
-        res.send({ user, token });
-    } catch (e) {
-        res.status(400).send(e);
-    }
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send(e);
+  }
 }
 
 async function logOut(req, res) {
-    try {
-        req.user.tokens = [];
-        await req.user.save();
+  try {
+    req.user.tokens = [];
+    await req.user.save();
 
-        res.send();
-    } catch (e) {
-        res.status(500).send(e);
-    }
-
+    res.send();
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
 
 async function myInfo(req, res) {
-    const _id = req.params.id;
+  const _id = req.params.id;
 
-    try {
-
-        const user = await User.findById(_id);
-        console.log(user);
-        if (!user) {
-            return res.status(404).send("This is a wrong id");
-        }
-        res.status(200).send(user);
-    } catch (e) {
-        res.status(404).send("This is a wrong id");
+  try {
+    const user = await User.findById(_id);
+    console.log(user);
+    if (!user) {
+      return res.status(404).send("This is a wrong id");
     }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(404).send("This is a wrong id");
+  }
 }
 
 async function updateInfo(req, res) {
-    const allowedUpdates = ['name', 'email', 'password'];
-    const updates = Object.keys(req.body);
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+  const allowedUpdates = ["name", "email", "password"];
+  const updates = Object.keys(req.body);
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
 
-    if (!isValidOperation) {
-        res.status(400).send({ error: 'Invalid Upadates!' });
-    }
+  if (!isValidOperation) {
+    res.status(400).send({ error: "Invalid Upadates!" });
+  }
 
-    try {
-        updates.forEach((update) => req.user[update] = req.body[update]);
-        await req.user.save();
-        res.send(req.user);
-
-    } catch (e) {
-        res.status(400).send(e);
-    }
+  try {
+    updates.forEach((update) => (req.user[update] = req.body[update]));
+    await req.user.save();
+    res.send(req.user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 }
 
 async function deleteUser(req, res) {
-    try {
-        await req.user.remove();
-        res.send(req.user);
-    } catch (e) {
-        res.status(500).send(e);
-    }
+  try {
+    await req.user.remove();
+    res.send(req.user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
 }
-
-
-
-
 
 // Function
 /* async function registerUser(req, res) {
@@ -122,9 +121,9 @@ async function deleteUser(req, res) {
 } */
 
 module.exports = {
-    register,
-    logIn,
-    logOut,
-    myInfo,
-    updateInfo
-}
+  register,
+  logIn,
+  logOut,
+  myInfo,
+  updateInfo,
+};
--- db/schema/user.js
+++ db/schema/user.js
@@ -1,39 +1,38 @@
-
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Schema = mongoose.Schema;
-const validator = require('validator')
+const validator = require("validator");
 
-let userSchema = new Schema({
+let userSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        unique: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Invalid Email.')
-            }
-
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email.");
         }
+      },
     },
     password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 5,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain password.')
-            }
-            /* add number in password
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 5,
+      validate(value) {
+        if (value.toLowerCase().includes("password")) {
+          throw new Error("Password cannot contain password.");
+        }
+        /* add number in password
             if(value.includes()){
                 throw new Error('Password need to contain a number.')
             } 
@@ -43,56 +42,60 @@ let userSchema = new Schema({
             } 
             
             */
-        }
+      },
     },
     firstname: { type: String, required: false },
     lastname: { type: String, required: false },
-    tokens: [{
+    tokens: [
+      {
         token: {
-            type: String,
-            required: true
-        }
-    }],
+          type: String,
+          required: true,
+        },
+      },
+    ],
     createIp: { type: String, required: false },
     isAdmin: { type: Number, required: false },
     resetPassword: { type: Object, required: false },
-    mobile: { type: Number, required: false }
-}, {
-    timestamps: true
-});
+    mobile: { type: Number, required: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.generateAuthToken = async function () {
-    const user = this
-    const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_KEY)
-    user.tokens = user.tokens.concat({ token })
-    await user.save()
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
 
-    return token
-}
+  return token;
+};
 userSchema.statics.findByCredentials = async (email, password) => {
-    const user = await User.findOne({ email })
-    console.log(user)
-    if (!user) {
-        throw new Error('Unable to login.')
-    }
+  const user = await User.findOne({ email });
+  console.log(user);
+  if (!user) {
+    throw new Error("Unable to login.");
+  }
 
-    const isMatch = await bcrypt.compare(password, user.password)
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) {
-        throw new Error('Unable to login.')
-    }
-    return user
-}
+  if (!isMatch) {
+    throw new Error("Unable to login.");
+  }
+  return user;
+};
 
-userSchema.pre('save', async function (next) {
-    const user = this
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8)
-    }
+userSchema.pre("save", async function (next) {
+  const user = this;
+  if (user.isModified("password")) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
 
-    next()
-})
+  next();
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User;  
+module.exports = User;
