60230fda6bff89194653927b7138a96e6f236e61
VicAffile <victoraffile@gmail.com>
2022-04-24 20:33:19 +0200

    add modele Serie et controller

---
+++ db/schema/serie.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Schema = mongoose.Schema;
+const validator = require("validator");
+
+let serieSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    numberBook: {
+      type: Number,
+      required: true,
+    },
+    picture: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Serie = mongoose.model("Serie", serieSchema);
+
+module.exports = Serie;
