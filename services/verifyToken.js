ef3775d4f94f328b9a9fd4e4310e964ff3dbf2f4
Bertyn Boulikou <bboulikou@gmail.com>
2022-04-25 14:47:48 +0200

    fix jwt

--- services/verifyToken.js
+++ services/verifyToken.js
@@ -1,23 +1,27 @@
 const User = require("../db/schema/user");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const verifyToken = async (req, res, next) => {
-    try {
-        const token = req.header('Authorization').replace('Bearer ', '')
-        const decoded = jwt.verify(token, process.env.JWT_KEY)
-        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
-        console.log(user)
-        if (!user) {
-            throw new Error()
-        }
-
-        req.token = token
-        req.user = user
-        next()
-    } catch (e) {
-        res.status(401).send({ error: 'Please authenticate.' })
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+    const user = await User.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
+    console.log(user);
+    if (!user) {
+      throw new Error();
     }
 
-}
+    req.token = token;
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+    res.status(401).send({ error: "Please authenticate." });
+  }
+};
 
 module.exports = verifyToken;
